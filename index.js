7706acae882056f22badcfae79835e6a6811297e
Angela-G9 <angelagathoni9568@gmail.com>
2024-11-26 16:01:43 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+let num1 = 31; 
+let num2 = 2;
+let multiply = num1 * num2;
+console.log( multiply );
+
+let random = Math.floor( Math.random() * 5) + 1;
+console.log(random);
+
+let num3 = 4;
+let num4 = 16;
+let mod = num3 % num4;
+console.log(mod)
+
+const max = Math.max(6,4,3,20,19,18,17)
+console.log(max);
+
+
+
